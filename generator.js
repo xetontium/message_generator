84d9c13642782aba8e0b06a849591861158c92db
xetontium <xetontium@gmail.com>
2023-07-21 18:56:42 +0200

    First commit locally

---
+++ generator.js
@@ -0,0 +1,5 @@
+const randomMessageGen = (name) => {
+    let positivemessage = ["“The secret of getting ahead is getting started“. —Mark Twain", "“I’ve missed more than 9,000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life, and that is why I succeed.” —Michael Jordan", "“Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve.” —Mary Kay Ash", "“It’s hard to beat a person who never gives up.” —Babe Ruth", "“We need to accept that we won’t always make the right decisions, that we’ll screw up royally sometimes―understanding that failure is not the opposite of success, it’s part of success.” ―Arianna Huffington"];
+    let // elgondolkodtató üzenetek
+    //valami személyes cucc
+}
